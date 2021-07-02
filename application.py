4aeac1effef6f9e1addf09da811b54521ed9f875
Dustin Vankrimpen <dustin@Dustins-Air.attlocal.net>
2021-07-01 22:22:07 -0400

    Added randomize feature to application

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from tkinter import *
 from joblib import load
+import random
 
 lr = load('lr-model.joblib')
 
 attributes = ["quantity_ethical","cash","quantity_acute","cheque","upi","quantity_ayurvedic","returned_quantity_bill","paytm","quantity_general","quantity_chronic","quantity_otc","total_spend_bill","return_value_bill","quantity_surgical","total_quantity_bill","phonepe","card","num_drugs_bill","quantity_generic","mrp_bill","transaction_count","quantity_h1"]
 window = tk.Tk()
+window.title('Customer Churn Prediction')
+prediction_text = "Press the 'Make Prediction' Button... "
 labels = []
 entries = []
 
@@ -15,16 +19,11 @@ entries = []
     #labels.append(label)
     #entries.append(entry)
 
-
 attribute_index = 0
-for i in range(5):
+for i in range(6):
     for j in range(5):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        frame.grid(row=i, column=j)
+        frame = LabelFrame(window,padx=5,pady=5,relief=FLAT)
+        frame.grid(row=i, column=j, sticky='EW')
         if (attribute_index < len(attributes)):
             label = tk.Label(master=frame, text=attributes[attribute_index])
             attribute_index += 1
@@ -33,22 +32,25 @@ for i in range(5):
             entry.insert(0,0)
             label.pack()
             entry.pack()
-   
-
-        if (i==4 and j==4):
-            button = tk.Button(
+        elif (attribute_index == len(attributes)):
+            frame.grid(row=i, column=j, columnspan=3)
+            prediction_label = tk.Label(master=frame, text="Press the 'Make Prediction' Button...", )
+            prediction_label.pack()
+            attribute_index += 1
+        if (i == 5 and j == 0):
+            prediction_button = tk.Button(
                 master=frame,
                 text="Make Prediction",
-                width=25,
-                height=5,
-                bg="blue",
-                fg="black",
+                activeforeground="red",
             )
-            button.pack()
-        
-
-prediction_label = tk.Label(master=frame)
-prediction_label.pack()
+            prediction_button.pack()
+        if (i == 5 and j == 1):
+            random_button = tk.Button(
+                master=frame,
+                text="Randomize...",
+                activeforeground="yellow",
+            )
+            random_button.pack()
 
 
 
@@ -59,12 +61,10 @@ def handle_keypress(event):
     name = entry.get()
     print(name)
 
-def handle_button(event):
+def make_prediction(event):
     row = []
     for thing in entries:
         row.append(float(thing.get()))
-        
-    
     prediction = lr.predict([[row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21]]])
     prediction_text = ""
     if (prediction == 0):
@@ -74,9 +74,43 @@ def handle_button(event):
     prediction_label.config(text=prediction_text)
     print(lr.predict([[row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21]]]))
 
+
+def randomize(event):
+    vals = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    vals[0] = random.uniform(0,500)
+    vals[1] = random.uniform(0,1)
+    vals[2] = random.uniform(0,1550)
+    vals[3] = random.uniform(0,1)
+    vals[4] = random.uniform(0,1)
+    vals[5] = random.uniform(0,23)
+    vals[6] = random.uniform(0,1514)
+    vals[7] = random.uniform(0,1)
+    vals[8] = random.uniform(0,366)
+    vals[9] = random.uniform(0,300)
+    vals[10] = random.uniform(0,40)
+    vals[11] = random.uniform(0.5,15621)
+    vals[12] = random.uniform(0,2573)
+    vals[13] = random.uniform(0,412)
+    vals[14] = random.uniform(0,1514)
+    vals[15] = random.uniform(0,1)
+    vals[16] = random.uniform(0,1)
+    vals[17] = random.uniform(0,48)
+    vals[18] = random.uniform(0,1000)
+    vals[19] = random.uniform(1,24118)
+    vals[20] = random.randrange(1,10)
+    vals[21] = random.uniform(0,56)
+
+    for i in range(len(entries)):
+        print(vals[i])
+        entries[i].delete(0,tk.END)
+        entries[i].insert(0, vals[i])
+        
+    
+  
    
 
-button.bind("<Button-1>", handle_button)
+prediction_button.bind("<Button-1>", make_prediction)
+random_button.bind("<Button-1>", randomize)
 # Bind keypress event to handle_keypress()
 window.bind("<Key>", handle_keypress)
 
